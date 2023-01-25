b61e58470cc81fce7bb09e84eae4b1a9bf66bbf6
Marcin Kackim <kacki.marcin@gmail.com>
2023-01-25 12:43:32 +0100

    type and category tag toggle buttons sort the database, increase and decrease the size of the database, added duplicate exclusion function

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-// const buttonCategoriesElement = document.querySelectorAll('.categories')
-// const buttonTypeElement = document.querySelectorAll('.type')
-// const buttonMovePictureElement = document.querySelectorAll('.movePicture')
-const tagElement = document.getElementById("tag");
-console.log(tagElement);
-
 function chooseCategoriesButton() {}
+let dataWithID = [];
 const getDataFromNasa = () => {
   fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=lxxVWb664W1Z1XEmHEsChUzLGFriIQKYZdXU8sdk&start_date=2022-06-20`
+    `https://api.nasa.gov/planetary/apod?api_key=lxxVWb664W1Z1XEmHEsChUzLGFriIQKYZdXU8sdk&start_date=2022-11-20`
   )
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      dataWithID = data.filter((element, index) => (element.id = index));
+      console.log(dataWithID);
+    })
     .catch(function (error) {
       console.log(error);
     });
 };
-
 getDataFromNasa();
 
-const tagPicture = [
+let tagPicture = [
   "Jupiter",
   "Comet",
   "Galaxy",
@@ -26,10 +23,44 @@ const tagPicture = [
   "Mars",
   "Sun",
   "Moon",
-  "Others",
+  "All",
 ];
+let tagType = ["image", "video"];
+let dataInsert = [];
 
-const tagType = ["image", "video"];
+function next(){
+  
+}
+function findTagInNasaData(dataWithID) {
+  let allTags = [...tagPicture, ...tagType];
+  dataInsert = [];
+  console.log(allTags);
+  dataWithID.forEach((element) => {
+    allTags.forEach((tag) => {
+      if (element.title.indexOf(tag) !== -1) {
+        dataInsert.push(element);
+      } else {
+        if (allTags.includes("All")) {
+          dataInsert.push(element);
+        }
+      }
+    });
+  });
+  removeDuplicate();  
+  return console.log(dataInsert);
+}
+
+function removeDuplicate(){
+  const uniqueIdsToDisplay = [];
+  dataInsert = dataInsert.filter((element) => {
+    const isDuplicate = uniqueIdsToDisplay.includes(element.id);
+    if (!isDuplicate) {
+      uniqueIdsToDisplay.push(element.id);
+      return true;
+    }
+    return false;
+  });
+}
 
 function addChild(parent, child, childClass, text, anotherChildClass) {
   const parentElement = document.querySelector(`${parent}`);
@@ -47,22 +78,43 @@ tagPicture.forEach((element) => {
 tagType.forEach((element) => {
   addChild(".tagType", "button", "button", element, "type");
 });
-
+//TODO proba stworzenia funkcji
+// function addToggleAndRemovedArrayTag(
+//   classElement,
+//   nameToggle,
+//   namearrayToRemove
+// ) {
+//   const getElement = document.querySelectorAll(`${classElement}`);
+//   getElement.forEach((picture) => {
+//     picture.addEventListener(`click`, function (event) {
+//       picture.classList.toggle("reductionVisibility");
+//       const nameButton = event.target.textContent;
+//       `${namearrayToRemove}`.includes(nameButton)
+//         ? (`${namearrayToRemove}`=`${namearrayToRemove}`.filter((item) => item !== nameButton))
+//         : `${namearrayToRemove}`.push(nameButton);
+//     });
+//   });
+// }
 const picturesElement = document.querySelectorAll(`.picture`);
 picturesElement.forEach((picture) => {
-  picture.addEventListener(`click`, function () {
-    picture.classList.toggle("smoof");
+  picture.addEventListener(`click`, function (event) {
+    picture.classList.toggle("reductionVisibility");
+    const nameButton = event.target.textContent;
+    tagPicture.includes(nameButton)
+      ? (tagPicture = tagPicture.filter((item) => item !== nameButton))
+      : tagPicture.push(nameButton);
+    findTagInNasaData(dataWithID);
   });
 });
 
 const typesElement = document.querySelectorAll(`.type`);
 typesElement.forEach((type) => {
-  type.addEventListener(`click`, function () {
-    type.classList.toggle("smoof");
+  type.addEventListener(`click`, function (event) {
+    type.classList.toggle("reductionVisibility");
+    const nameButton = event.target.textContent;
+    tagType.includes(nameButton)
+      ? (tagType = tagType.filter((item) => item !== nameButton))
+      : tagType.push(nameButton);
+    findTagInNasaData(dataWithID);
   });
 });
-// picturesElement.forEach((picture) => {
-//   picture.addEventListener(`click`, function () {
-//     picture.classList.toggle("smoof");
-//   });
-// });

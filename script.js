0a1cd30f75bc15056ee19c367a6571609ee0fd22
Marcin Kackim <kacki.marcin@gmail.com>
2023-01-26 13:42:22 +0100

    all functionalities implemented. code is before refactor

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function chooseCategoriesButton() {}
 let dataWithID = [];
 const getDataFromNasa = () => {
   fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=lxxVWb664W1Z1XEmHEsChUzLGFriIQKYZdXU8sdk&start_date=2022-11-20`
+    `https://api.nasa.gov/planetary/apod?api_key=lxxVWb664W1Z1XEmHEsChUzLGFriIQKYZdXU8sdk&start_date=2022-01-01`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -30,37 +30,129 @@ let tagPicture = [
 let tagType = ["image", "video"];
 let dataInsert = [];
 let index = 0;
+const nextButtonElement = document.querySelector(".next");
+const prevButtonElement = document.querySelector(".prev");
+const buttonMovePictureElement = document.querySelector(".movePicture");
 
 function next() {
-  const nextButtonElement = document.querySelector(".next");
   nextButtonElement.addEventListener("click", function () {
-    index++;
-    insertPictureOrMovies(dataInsert, index);
-    console.log(index);
-    console.log("test");
+    if (index < dataInsert.length - 1) {
+      index++;
+    }
+    if (index !== 0) {
+      prevButtonElement.classList.remove("reductionVisibilityNextPrevButton");
+      nextButtonElement.classList.remove("reductionVisibilityNextPrevButton");
+    }
+    console.log(dataInsert.length);
+    if (index === dataInsert.length - 1) {
+      nextButtonElement.classList.add("reductionVisibilityNextPrevButton");
+    }
+    if (index <= dataInsert.length - 1) {
+      insertPictureOrMovies(dataInsert, index);
+      console.log(index);
+      console.log("test");
+    }
+  });
+}
+
+function prev() {
+  prevButtonElement.addEventListener("click", function () {
+    if (index > 0) {
+      index--;
+    }
+    if (index < dataInsert.length - 1) {
+      prevButtonElement.classList.remove("reductionVisibilityNextPrevButton");
+      nextButtonElement.classList.remove("reductionVisibilityNextPrevButton");
+    }
+    console.log(dataInsert.length);
+    if (index === 0) {
+      prevButtonElement.classList.add("reductionVisibilityNextPrevButton");
+    }
+    if (index <= dataInsert.length - 1) {
+      insertPictureOrMovies(dataInsert, index);
+      console.log(index);
+      console.log("test");
+    }
   });
 }
 
 function insertPictureOrMovies(data, index) {
+  let elementToRemoveA = document.querySelector("a");
+  if (elementToRemoveA) {
+    elementToRemoveA.remove();
+  }
+  let elementToRemoveIframe = document.querySelector("iframe");
+  if (elementToRemoveIframe) {
+    elementToRemoveIframe.remove();
+  }
+  let elementToRemoveSection = document.querySelector(".description");
+  if (elementToRemoveSection) {
+    elementToRemoveSection.remove();
+  }
+  data[index].media_type === "image"
+    ? insertPicture(data, index)
+    : insertMovie(data, index);
+  insertDescription(data, index);
+}
+
+function insertMovie(data, index) {
   let elementToRemove = document.querySelector("a");
   if (elementToRemove) {
     elementToRemove.remove();
   }
   const parent = document.querySelector("section");
+  const child = document.createElement("iframe");
+  child.className = "secondDisplay";
+  child.src = `${data[index].url}`;
+  child.frameborder = "0";
+  child.allow =
+    "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+  child.allowfullscreen = true;
+  parent.insertBefore(child, parent.firstChild);
+}
+
+function insertPicture(data, index) {
+  const parent = document.querySelector("section");
   const child = document.createElement("a");
   child.href = `${data[index].url}`;
   child.target = "_blank";
+  child.className = "secondDisplay";
   const img = document.createElement("img");
   img.src = `${data[index].url}`;
   img.alt = "Dew drop";
   child.appendChild(img);
   parent.insertBefore(child, parent.firstChild);
 }
+function insertDescription(data, index) {
+  const parent = document.querySelector("section");
+  const newSection = document.createElement("section");
+  newSection.className = "description";
+  const h2 = document.createElement("h2");
+  h2.className = "title";
+  const h2Text = `${data[index].title}`;
+  let i = 0;
+  const interval = setInterval(() => {
+    h2.textContent += h2Text[i];
+    i++;
+    if (i === h2Text.length) {
+      clearInterval(interval);
+    }
+  }, 100);
+
+  const p = document.createElement("p");
+  p.className = "explanation";
+  const pText = document.createTextNode(`${data[index].explanation}`);
+  p.appendChild(pText);
+  newSection.appendChild(h2);
+  newSection.appendChild(p);
+  parent.insertBefore(newSection, parent.firstChild);
+}
 
 next();
+prev();
 //todo
 function findTagInNasaData(dataWithID) {
-  index = 0
+  index = 0;
   let allTags = [...tagPicture, ...tagType];
   dataInsert = [];
   console.log(allTags);
@@ -76,8 +168,11 @@ function findTagInNasaData(dataWithID) {
     });
   });
   removeDuplicate();
+
+  dataInsert.length === 0
+    ? nextButtonElement.classList.add("reductionVisibilityNextPrevButton")
+    : nextButtonElement.classList.remove("reductionVisibilityNextPrevButton");
   insertPictureOrMovies(dataInsert, 0);
-  console.log(dataInsert);
 
   return dataInsert;
 }
@@ -135,6 +230,7 @@ picturesElement.forEach((picture) => {
     tagPicture.includes(nameButton)
       ? (tagPicture = tagPicture.filter((item) => item !== nameButton))
       : tagPicture.push(nameButton);
+
     findTagInNasaData(dataWithID);
   });
 });

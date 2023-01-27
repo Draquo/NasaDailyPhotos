9d12ca7d59b8c853501e2d7a7f66a5ae6af52840
Marcin Kackim <kacki.marcin@gmail.com>
2023-01-27 11:07:15 +0100

    refacor code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-function chooseCategoriesButton() {}
 let dataWithID = [];
 const getDataFromNasa = () => {
   fetch(
@@ -30,198 +29,36 @@ let tagPicture = [
 let tagType = ["image", "video"];
 let dataInsert = [];
 let index = 0;
+const parent = document.querySelector("section");
+const child = document.createElement("iframe");
 const nextButtonElement = document.querySelector(".next");
 const prevButtonElement = document.querySelector(".prev");
 const buttonMovePictureElement = document.querySelector(".movePicture");
+const cardClassElement = document.querySelector(".card");
 
-function next() {
-  nextButtonElement.addEventListener("click", function () {
-    if (index < dataInsert.length - 1) {
-      index++;
-    }
-    if (index !== 0) {
-      prevButtonElement.classList.remove("reductionVisibilityNextPrevButton");
-      nextButtonElement.classList.remove("reductionVisibilityNextPrevButton");
-    }
-    console.log(dataInsert.length);
-    if (index === dataInsert.length - 1) {
-      nextButtonElement.classList.add("reductionVisibilityNextPrevButton");
-    }
-    if (index <= dataInsert.length - 1) {
-      insertPictureOrMovies(dataInsert, index);
-      console.log(index);
-      console.log("test");
-    }
-  });
-}
-
-function prev() {
-  prevButtonElement.addEventListener("click", function () {
-    if (index > 0) {
-      index--;
-    }
-    if (index < dataInsert.length - 1) {
-      prevButtonElement.classList.remove("reductionVisibilityNextPrevButton");
-      nextButtonElement.classList.remove("reductionVisibilityNextPrevButton");
-    }
-    console.log(dataInsert.length);
-    if (index === 0) {
-      prevButtonElement.classList.add("reductionVisibilityNextPrevButton");
-    }
-    if (index <= dataInsert.length - 1) {
-      insertPictureOrMovies(dataInsert, index);
-      console.log(index);
-      console.log("test");
-    }
-  });
-}
-
-function insertPictureOrMovies(data, index) {
-  let elementToRemoveA = document.querySelector("a");
-  if (elementToRemoveA) {
-    elementToRemoveA.remove();
-  }
-  let elementToRemoveIframe = document.querySelector("iframe");
-  if (elementToRemoveIframe) {
-    elementToRemoveIframe.remove();
-  }
-  let elementToRemoveSection = document.querySelector(".description");
-  if (elementToRemoveSection) {
-    elementToRemoveSection.remove();
-  }
-  data[index].media_type === "image"
-    ? insertPicture(data, index)
-    : insertMovie(data, index);
-  insertDescription(data, index);
-}
-
-function insertMovie(data, index) {
-  let elementToRemove = document.querySelector("a");
-  if (elementToRemove) {
-    elementToRemove.remove();
-  }
-  const parent = document.querySelector("section");
-  const child = document.createElement("iframe");
-  child.className = "secondDisplay";
-  child.src = `${data[index].url}`;
-  child.frameborder = "0";
-  child.allow =
-    "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
-  child.allowfullscreen = true;
-  parent.insertBefore(child, parent.firstChild);
-}
-
-function insertPicture(data, index) {
-  const parent = document.querySelector("section");
-  const child = document.createElement("a");
-  child.href = `${data[index].url}`;
-  child.target = "_blank";
-  child.className = "secondDisplay";
-  const img = document.createElement("img");
-  img.src = `${data[index].url}`;
-  img.alt = "Dew drop";
-  child.appendChild(img);
-  parent.insertBefore(child, parent.firstChild);
-}
-function insertDescription(data, index) {
-  const parent = document.querySelector("section");
-  const newSection = document.createElement("section");
-  newSection.className = "description";
-  const h2 = document.createElement("h2");
-  h2.className = "title";
-  const h2Text = `${data[index].title}`;
-  let i = 0;
-  const interval = setInterval(() => {
-    h2.textContent += h2Text[i];
-    i++;
-    if (i === h2Text.length) {
-      clearInterval(interval);
-    }
-  }, 100);
-
-  const p = document.createElement("p");
-  p.className = "explanation";
-  const pText = document.createTextNode(`${data[index].explanation}`);
-  p.appendChild(pText);
-  newSection.appendChild(h2);
-  newSection.appendChild(p);
-  parent.insertBefore(newSection, parent.firstChild);
-}
-
-next();
-prev();
-//todo
-function findTagInNasaData(dataWithID) {
-  index = 0;
-  let allTags = [...tagPicture, ...tagType];
-  dataInsert = [];
-  console.log(allTags);
-  dataWithID.forEach((element) => {
-    allTags.forEach((tag) => {
-      if (element.title.indexOf(tag) !== -1) {
-        dataInsert.push(element);
-      } else {
-        if (allTags.includes("All")) {
-          dataInsert.push(element);
-        }
-      }
-    });
-  });
-  removeDuplicate();
-
-  dataInsert.length === 0
-    ? nextButtonElement.classList.add("reductionVisibilityNextPrevButton")
-    : nextButtonElement.classList.remove("reductionVisibilityNextPrevButton");
-  insertPictureOrMovies(dataInsert, 0);
-
-  return dataInsert;
-}
-
-function removeDuplicate() {
-  const uniqueIdsToDisplay = [];
-  dataInsert = dataInsert.filter((element) => {
-    const isDuplicate = uniqueIdsToDisplay.includes(element.id);
-    if (!isDuplicate) {
-      uniqueIdsToDisplay.push(element.id);
-      return true;
-    }
-    return false;
-  });
-}
-
-function addChild(parent, child, childClass, text, anotherChildClass) {
-  const parentElement = document.querySelector(`${parent}`);
-  const childElement = document.createElement(`${child}`);
-  childElement.classList.add(`${childClass}`);
-  childElement.classList.add(`${anotherChildClass}`);
-  childElement.innerHTML = `${text}`;
+function createChildElement(
+  getParentElement,
+  createChild,
+  setChildClass,
+  setText,
+  SetAnotherChildClass
+) {
+  const parentElement = document.querySelector(`${getParentElement}`);
+  const childElement = document.createElement(`${createChild}`);
+  childElement.classList.add(`${setChildClass}`);
+  childElement.classList.add(`${SetAnotherChildClass}`);
+  childElement.innerHTML = `${setText}`;
   parentElement.appendChild(childElement);
 }
 
 tagPicture.forEach((element) => {
-  addChild(".tagPicture", "button", "button", element, "picture");
+  createChildElement(".tagPicture", "button", "button", element, "picture");
 });
 
 tagType.forEach((element) => {
-  addChild(".tagType", "button", "button", element, "type");
+  createChildElement(".tagType", "button", "button", element, "type");
 });
-//TODO proba stworzenia funkcji
-// function addToggleAndRemovedArrayTag(
-//   classElement,
-//   nameToggle,
-//   namearrayToRemove
-// ) {
-//   const getElement = document.querySelectorAll(`${classElement}`);
-//   getElement.forEach((picture) => {
-//     picture.addEventListener(`click`, function (event) {
-//       picture.classList.toggle("reductionVisibility");
-//       const nameButton = event.target.textContent;
-//       `${namearrayToRemove}`.includes(nameButton)
-//         ? (`${namearrayToRemove}`=`${namearrayToRemove}`.filter((item) => item !== nameButton))
-//         : `${namearrayToRemove}`.push(nameButton);
-//     });
-//   });
-// }
+
 const picturesElement = document.querySelectorAll(`.picture`);
 picturesElement.forEach((picture) => {
   picture.addEventListener(`click`, function (event) {
@@ -246,3 +83,152 @@ typesElement.forEach((type) => {
     findTagInNasaData(dataWithID);
   });
 });
+
+function next() {
+  nextButtonElement.addEventListener("click", function () {
+    if (index < dataInsert.length - 1) {
+      index++;
+    }
+    if (index !== 0) {
+      prevButtonElement.classList.remove("reductionVisibilityNextPrevButton");
+      nextButtonElement.classList.remove("reductionVisibilityNextPrevButton");
+    }
+    if (index === dataInsert.length - 1) {
+      nextButtonElement.classList.add("reductionVisibilityNextPrevButton");
+    }
+    if (index <= dataInsert.length - 1) {
+      insertCardsOnDisplay(dataInsert, index);
+    }
+  });
+}
+
+function prev() {
+  prevButtonElement.addEventListener("click", function () {
+    if (index > 0) {
+      index--;
+    }
+    if (index < dataInsert.length - 1) {
+      prevButtonElement.classList.remove("reductionVisibilityNextPrevButton");
+      nextButtonElement.classList.remove("reductionVisibilityNextPrevButton");
+    }
+    if (index === 0) {
+      prevButtonElement.classList.add("reductionVisibilityNextPrevButton");
+    }
+    if (index <= dataInsert.length - 1) {
+      insertCardsOnDisplay(dataInsert, index);
+    }
+  });
+}
+next();
+prev();
+
+function insertCardsOnDisplay(data, index) {
+  cardClassElement.innerHTML = "";
+  data[index].media_type === "image"
+    ? insertPictureCard(data, index)
+    : insertMovieCard(data, index);
+  insertDescriptionCard(data, index);
+}
+
+function insertMovieCard(data, index) {
+  const child = document.createElement("iframe");
+  child.className = "secondDisplay";
+  child.src = `${data[index].url}`;
+  child.frameborder = "0";
+  child.allow =
+    "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+  child.allowfullscreen = true;
+  parent.insertBefore(child, parent.firstChild);
+}
+
+function insertPictureCard(data, index) {
+  const child = document.createElement("a");
+  child.href = `${data[index].url}`;
+  child.target = "_blank";
+  child.className = "secondDisplay";
+  const img = document.createElement("img");
+  img.src = `${data[index].url}`;
+  img.alt = "Dew drop";
+  child.appendChild(img);
+  parent.insertBefore(child, parent.firstChild);
+}
+function insertDescriptionCard(data, index) {
+  const newSection = document.createElement("section");
+  newSection.className = "description";
+  const h2 = document.createElement("h2");
+  h2.className = "title";
+  const h2Text = `${data[index].title}`;
+  let i = 0;
+  const interval = setInterval(() => {
+    h2.textContent += h2Text[i];
+    i++;
+    if (i === h2Text.length) {
+      clearInterval(interval);
+    }
+  }, 100);
+
+  const p = document.createElement("p");
+  p.className = "explanation";
+  const pText = document.createTextNode(`${data[index].explanation}`);
+  p.appendChild(pText);
+  newSection.appendChild(h2);
+  newSection.appendChild(p);
+  parent.insertBefore(newSection, parent.firstChild);
+}
+
+//todo
+function findTagInNasaData(dataWithID) {
+  index = 0;
+  let allTags = [...tagPicture, ...tagType];
+  dataInsert = [];
+  console.log(allTags);
+  dataWithID.forEach((element) => {
+    allTags.forEach((tag) => {
+      if (element.title.indexOf(tag) !== -1) {
+        dataInsert.push(element);
+      } else {
+        if (allTags.includes("All")) {
+          dataInsert.push(element);
+        }
+      }
+    });
+  });
+  removeDuplicate();
+  dataInsert.length === 0
+    ? nextButtonElement.classList.add("reductionVisibilityNextPrevButton")
+    : nextButtonElement.classList.remove("reductionVisibilityNextPrevButton");
+  insertCardsOnDisplay(dataInsert, 0);
+  return dataInsert;
+}
+
+function removeDuplicate() {
+  const uniqueIdsToDisplay = [];
+  dataInsert = dataInsert.filter((element) => {
+    const isDuplicate = uniqueIdsToDisplay.includes(element.id);
+    if (!isDuplicate) {
+      uniqueIdsToDisplay.push(element.id);
+      return true;
+    }
+    return false;
+  });
+}
+
+// toggleButtom(".type", tagType)
+// toggleButtom(".picture", tagPicture)
+
+// function toggleButtom(classOfButtonElement, arrayWithTags){
+
+// const buttomsElement = document.querySelectorAll(classOfButtonElement);
+// buttomsElement.forEach((button) => {
+//   button.addEventListener(`click`, function (event) {
+//     button.classList.toggle("reductionVisibility");
+//     const nameButton = event.target.textContent;
+//     console.log(arrayWithTags);
+//     arrayWithTags.includes(nameButton)
+//       ? (arrayWithTags = arrayWithTags.filter((item) => item !== nameButton))
+//       : arrayWithTags.push(nameButton);
+//     findTagInNasaData(dataWithID);
+//   });
+// });
+
+// }
